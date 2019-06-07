011d86f7b008bb1863633ed825b7156a36877e7a
Alpha Man <xalphamanx@tuta.io>
2019-06-07 15:25:39 +0530

    Added google analytics

--- Server/Routes/web.php
+++ Server/Routes/web.php
@@ -11,6 +11,5 @@ $app->post('/link/new', LinkController::class . ':newLink')
 
 $app->get('/s/{code}', LinkController::class . ':getLinkS2U')
     ->setName('link.show.s2u');
-
 $app->get('/{code}', LinkController::class . ':getLink')
     ->setName('link.show');
